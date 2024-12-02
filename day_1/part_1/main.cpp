9da0e852f1f2f755218ab7686cd10c227f6b79ea
Jannick M <jannick.megens@student.hu.nl>
2024-12-03 00:24:52 +0100

    renamed ids 1 and 2 to l and r

--- day_1/part_1/main.cpp
+++ day_1/part_1/main.cpp
@@ -6,8 +6,8 @@
 #include <sstream>
 
 int main() {
-    std::vector<unsigned long> location_ids1 = {}; 
-    std::vector<unsigned long> location_ids2 = {}; 
+    std::vector<unsigned long> location_ids_l = {}; 
+    std::vector<unsigned long> location_ids_r = {}; 
 
     std::string line;
     std::ifstream file("input.txt");
@@ -22,18 +22,18 @@ int main() {
             
             // Extract two integers from the stringstream
             ss >> num1 >> num2;
-            location_ids1.push_back(num1);
-            location_ids2.push_back(num2);
+            location_ids_l.push_back(num1);
+            location_ids_r.push_back(num2);
         }
         file.close();
     }
 
-    std::sort(location_ids1.begin(), location_ids1.end());
-    std::sort(location_ids2.begin(), location_ids2.end());
+    std::sort(location_ids_l.begin(), location_ids_l.end());
+    std::sort(location_ids_r.begin(), location_ids_r.end());
 
     int total_diff = 0;
-    for (unsigned long i = 0; i < location_ids1.size(); i++) {
-        unsigned long diff = std::llabs(location_ids1[i] - location_ids2[i]);
+    for (unsigned long i = 0; i < location_ids_l.size(); i++) {
+        unsigned long diff = std::llabs(location_ids_l[i] - location_ids_r[i]);
         total_diff += diff;
     }
 
